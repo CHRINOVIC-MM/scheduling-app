f86d296fd92d34b10aae4a5beca9c25f4e573526
= <=>
2023-06-22 09:03:26 +0200

    {server} setup server listening

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/api", (req, res) =>{
+    res.json({
+        message: "Hello world",
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`server listening on ${PORT}`);
+});
