b21a191f8b9304da80f181f3fe47852f9c511e58
= <=>
2023-06-22 09:31:47 +0200

    {front-end} add basic structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// component
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
+import BookUser from "./components/BookUser";
+// React-Toastify configuration
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login />} />
+                    <Route path='/register' element={<Signup />} />
+                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/profile/:id' element={<Profile />} />
+                    <Route path='/book/:user' element={<BookUser />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer />
+        </div>
+    );
+};
+export default App;
